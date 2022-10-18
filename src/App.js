27ad82c7f993fac243725648683be5af3a64a6b0
arefinnuman <arefinnuman2000@gmail.com>
2022-10-19 02:12:19 +0600

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./Pages/Shared/Header/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fc593c",
+
+          secondary: "#a9e057",
+
+          accent: "#fff2b5",
+
+          neutral: "#282C39",
+
+          "base-100": "#E0DDEE",
+
+          info: "#4973DF",
+
+          success: "#41D279",
+
+          warning: "#E0B415",
+
+          error: "#E8547E",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
